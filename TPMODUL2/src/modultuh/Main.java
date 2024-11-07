e088131b1aa390fb81a3a60ea4eaa7ba8d8fbf9f
jioxy <khalilaazizagunawan@gmail.com>
2024-11-07 22:31:50 +0700

    new file:   TPMODUL2/.vscode/settings.json 	new file:   TPMODUL2/README.md 	new file:   TPMODUL2/src/modultuh/Burung.java 	new file:   TPMODUL2/src/modultuh/Hewan.java 	new file:   TPMODUL2/src/modultuh/Kucing.java 	new file:   TPMODUL2/src/modultuh/Main.java

---
+++ TPMODUL2/src/modultuh/Main.java
@@ -0,0 +1,20 @@
+package modultuh;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Detail Hewan\n");
+        Kucing kucing = new Kucing("asep", 3, "Persia");
+        System.out.println("ini adalah " + kucing.nama + "!");
+        kucing.suara();
+        kucing.makan();
+        kucing.makan("ikan asin");
+        kucing.infoHewan();
+
+        Burung burung = new Burung("alibaba", 1, "merah");
+        System.out.println("\nini adalah " + burung.nama + "!");
+        burung. suara();
+        burung.makan();
+        burung.makan("kacang");
+        burung.infoHewan();
+    }
+}
